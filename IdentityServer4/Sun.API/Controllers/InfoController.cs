a3b941fe93ed4abade857f81824840f063169733
孙贵垚 <17316221024@163.com>
2018-01-12 12:51:32 +0800

    IdentityServer4Case

---
+++ IdentityServer4/Sun.API/Controllers/InfoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Sun.API.Controllers
+{
+    [Route("[controller]")]
+    [Authorize]
+    public class InfoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Json(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
---
+++ IdentityServer4/Sun.MVCClient/Controllers/InfoController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sun.MVCClient.Models;
+using IdentityModel;
+using IdentityModel.Client;
+using System.Net.Http;
+namespace Sun.MVCClient.Controllers
+{
+    public class InfoController : Controller
+    {
+        public async Task<TokenResponse> GetToken()
+        {
+            var disco = await DiscoveryClient.GetAsync("http://localhost:49173");
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "Secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            if (tokenResponse.IsError)
+            {
+                return null;
+            }
+
+
+            return tokenResponse;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var tokenResponse = await GetToken();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:49559/Info");
+            if (!response.IsSuccessStatusCode) return BadRequest();
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return Json(content);
+            }
+        }
+    }
+}
